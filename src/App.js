1434924e636572b6464353545d556382bac6aa32
Alessia Piscopo <alessia.piscopo@hotmail.com>
2022-10-18 21:25:26 -0400

    Add functionality to fetch random word from solutions array

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [solution, setSolution] = useState(null);
+
+  // fetch a random word
+  useEffect(() => {
+    fetch("http://localhost:5000/solutions")
+      .then((res) => res.json())
+      .then((json) => {
+        // generate random integer between 0 and length of array
+        const randomSolution = json[Math.floor(Math.random() * json.length)];
+        setSolution(randomSolution.word);
+      });
+  }, [setSolution]);
+
   return (
     <div className="App">
-      <h1>Wordle (Lingo)</h1>
+      <h1>Wordle</h1>
+      {solution && <div>Solution is: {solution}</div>}
     </div>
   );
 }
